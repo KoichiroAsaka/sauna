bf958a7218c4663f80d799421fa4817e4078b6ff
Koichiro Asaka <KoichiroAsaka>
2025-11-27 11:24:22 +0900

    sceneのseed反映

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,45 @@
 Sauna.create!([
   {
     name: "サウナしきじ",
-    prefecture: "shizuoka",
+    prefecture: :shizuoka,
     address: "静岡県静岡市駿河区敷地2-25-1"
   },
   {
     name: "ふきさらし湯",
-    prefecture: "shizuoka",
-    address: "静岡県静岡市駿河区丸子3288-2" 
+    prefecture: :shizuoka,
+    address: "静岡県静岡市駿河区丸子3288-2"
   },
   {
     name: "CYCL",
-    prefecture: "yamanashi",
+    prefecture: :yamanashi, 
     address: "山梨県南都留郡山中湖村平野479-107"
   },
   {
     name: "サウナ煌-KOU-",
-    prefecture: "shizuoka",
+    prefecture: :shizuoka,
     address: "静岡市清水区草薙1-2-26"
   },
   {
     name: "サウナ東京",
-       prefecture: "tokyo",
+    prefecture: :tokyo,
     address: "東京都港区赤坂3-13-4"
   },
   {
     name: "かるまる",
-    prefecture: "tokyo",
+    prefecture: :tokyo,
     address: "東京都豊島区池袋2丁目7-7 6階"
   },
   {
     name: "KIWAMI SAUNA",
-    prefecture: "aichi",
+    prefecture: :aichi,
     address: "愛知県名古屋市西区浅間2丁目14-24"
   }
+])
+
+Scene.create!([
+  { description: "サウナ室で友達と話す" },
+  { description: "サウナ室でサウナマットを敷かない" },
+  { description: "汗を流さずに水風呂へ入水" },
+  { description: "水風呂に頭から潜水" }
 ])
 
